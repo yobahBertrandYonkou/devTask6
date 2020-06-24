9b0dfa6f07aff603aad39f5718ddfc7ac8cdf2bb
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2020-06-24 23:09:47 +0530

    Add files via upload

---
+++ task6seedjob.groovy
@@ -0,0 +1,41 @@
+job("contLauncher") {
+
+    description("checks code language and launches the respective pod")
+
+    //sets a custom work space
+    customWorkspace("/var/lib/jenkins/workspace/devtask6")
+
+    //executes commands on shell
+    steps{
+        shell("s")
+        shell("s")
+        shell()
+        shell()
+        shell()
+        shell()
+    }
+
+    //triggers website tester on success
+    publishers{
+    downstream("websiterTester", "SUCCESS")
+    }
+}
+
+job("websiterTester"){
+    description("connects to website and gets status code")
+
+    //sets a custom work space
+    customWorkspace("/var/lib/jenkins/workspace/devtask6")
+
+    //executes on shell
+    steps{
+        shell()
+        shell()
+        shell()
+        shell()
+        shell()
+        shell()
+        shell()
+    }
+}
+
